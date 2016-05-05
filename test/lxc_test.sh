de47cc993e063716975539c663886023d163c6f1
Stefan Berggren <nsg@nsg.cc>
2016-05-05 11:02:31 +0200

    foo

--- test/lxc_test.sh
+++ test/lxc_test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get install -y \
+	-o Dpkg::Options::="--force-confdef" \
+	-o Dpkg::Options::="--force-confold" \
+	lxc debootstrap
+
 sudo lxc-create -t debian -n debian8
